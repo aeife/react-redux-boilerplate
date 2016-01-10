f9efaa19e04d30410329112a1041046f10f1a369
André Eife <andre.eife@gmail.com>
2016-01-10 14:52:59 +0100

    added history api fallback to dev server for html5 routes

--- server/server.js
+++ server/server.js
@@ -4,19 +4,18 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from '../build/webpack.config.babel.js';
+import fallback from 'connect-history-api-fallback';
 
 const app = express();
 const compiler = webpack(config);
 
+app.use(fallback())
 app.use(express.static(path.resolve('./dist')));
 app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/',
-  headers: { "X-Custom-Header": "yes" },
   stats: {
     colors: true
   }
 }));
-
 app.use(webpackHotMiddleware(compiler));
 
 app.listen(8080, '0.0.0.0');
