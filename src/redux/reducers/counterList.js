b8a7524a1c2352beac5daa5280a34ca0417bf001
André Eife <andre.eife@gmail.com>
2016-01-14 00:53:22 +0100

    typo

--- src/redux/reducers/counterList.js
+++ src/redux/reducers/counterList.js
@@ -2,9 +2,9 @@ import { handleActions } from 'redux-actions';
 import { ADD_COUNTER } from 'redux/actions/counterList';
 import { COUNTER_INCREMENT } from 'redux/actions/counter';
 import { List } from 'immutable';
-import counterReduxer from './counter';
+import counterReducer from './counter';
 
 export default handleActions({
-  [ADD_COUNTER]: (state, action) => state.push(counterReduxer(undefined, action)),
-  [COUNTER_INCREMENT]: (state, action) => state.map(counter => counterReduxer(counter, action))
+  [ADD_COUNTER]: (state, action) => state.push(counterReducer(undefined, action)),
+  [COUNTER_INCREMENT]: (state, action) => state.map(counter => counterReducer(counter, action))
 }, List());
