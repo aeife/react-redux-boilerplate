a467a0d3564a1d35dc816b321a764d1e13f343d8
André Eife <andre.eife@gmail.com>
2016-03-01 15:31:30 +0100

    do not use arrow functions for actions

--- src/redux/actions/counter.js
+++ src/redux/actions/counter.js
@@ -1,6 +1,8 @@
 export const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
 
-export const increment = (id = 1, value = 1) => ({type: COUNTER_INCREMENT, payload: {id, value}});
+export const increment = function (id = 1, value = 1) {
+    return {type: COUNTER_INCREMENT, payload: {id, value}};
+}
 
 export const actions = {
   increment
--- src/redux/actions/counterList.js
+++ src/redux/actions/counterList.js
@@ -1,7 +1,9 @@
 export const ADD_COUNTER = 'ADD_COUNTER';
 
 let nextCounterId = 0;
-export const addCounter = value => ({type: ADD_COUNTER, payload: {...value, id: nextCounterId++}});
+export const addCounter = function (value) {
+    return {type: ADD_COUNTER, payload: {...value, id: nextCounterId++}};
+};
 
 export const actions = {
   addCounter
